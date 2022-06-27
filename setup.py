609ed77c46812004be339878d3349c995dfab6dd
Ruby <write2ruby@gmail.com>
2022-06-27 16:23:48 +0530

    Issue

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+from typing import List
+
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictorr"
+VERSION="0.0.6"
+AUTHOR="Ruby"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+   
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
+)
