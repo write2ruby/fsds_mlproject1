e8a3601802550a16d023740a3fa1b55fe2213c69
Ruby <write2ruby@gmail.com>
2022-07-02 21:57:08 +0530

    July2 v1

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -1,12 +1,10 @@
 import sys
 import os
-import sys
 
 class HousingException(Exception):
 
   def __init__(self, error_message:Exception, error_detail:sys):
     super().__init__(error_message)
-    Exception(error_message)
     self.error_message=HousingException.get_detailed_error_message(error_message=error_message, error_detail=error_detail)
 
   @staticmethod
@@ -16,12 +14,9 @@ class HousingException(Exception):
     error_detail: object of sys module
     """
     _,_,exec_tb = error_detail.exc_info()
-
-    file_name = exec_tb.tb_frame.f_lineno
+    line_number = exec_tb.tb_frame.f_lineno
     file_name = exec_tb.tb_frame.f_code.co_filename
-
     error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
-
     return error_message
 
   def __str__(self):
